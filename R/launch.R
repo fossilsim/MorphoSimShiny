8cc04e96c1e7a804b5a791a9c727bdb38aa16db1
Tim Brandler <tim.brandler@gmail.com>
2024-12-02 15:48:13 +0100

    changes to launch function

--- R/launch.R
+++ R/launch.R
@@ -13,7 +13,7 @@
 #' @export
 
 launchMorphoSimShiny <- function(inbrowser = TRUE) {
-  appDir <- system.file("shinyApp", package = "MorphoSimShiny")
+  appDir <- system.file("app.R", package = "MorphoSimShiny")
   if (appDir == "") {
     stop("Could not find shinyApp. Try re-installing `MorphoSimShiny`.", call. = FALSE)
   }
